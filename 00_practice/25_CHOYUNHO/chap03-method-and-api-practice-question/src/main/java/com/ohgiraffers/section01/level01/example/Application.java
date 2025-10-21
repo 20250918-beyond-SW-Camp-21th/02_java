41b58984f787fa9f60d3fba67a8cfa2c1ece861b
cho-yunho01 <04whdbsgh30@naver.com>
2025-10-21 14:02:08 +0900

    random2

---
+++ 00_practice/25_CHOYUNHO/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/example/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section01.level01.example;
+
+public class Application {
+    public static void main(String[] args) {
+        ExampleTest et = new ExampleTest();
+        et.even0dd(10);
+        et.findMax(10,20);
+        et.randomMathQuiz();
+    }
+}
--- 00_practice/25_CHOYUNHO/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/Application.java
+++ 00_practice/25_CHOYUNHO/chap03-method-and-api-practice-question/src/main/java/com/ohgiraffers/section01/level01/normal/Application.java
@@ -1,11 +1,13 @@
 package com.ohgiraffers.section01.level01.normal;
 
+import static com.ohgiraffers.section01.level01.normal.RandomMaker.tossCoin;
+
 public class Application {
 
     public static void main(String[] args) {
 
         // 실행용 클래스
-        RandomMaker randomMaker = new RandomMaker();
-        System.out.println(randomMaker.tossCoin());
+        // RandomMaker randomMaker = new RandomMaker();
+        System.out.println(tossCoin());
     }
 }
